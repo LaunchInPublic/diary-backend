1ec23034ce4c4546a368b55a66be23058c6dbe84
Pierre-Henry Soria <pierre@ph7.me>
2024-06-23 18:42:38 +1000

    Add ID to each note + Retrieve note per their ID

--- routes/diary.route.ts
+++ routes/diary.route.ts
@@ -4,10 +4,10 @@ import { StatusCodes } from "http-status-codes";
 const dynamicDbData: any = [];
 
 interface Diary {
-  id: string;
+  id?: number;
   title: string;
   content: string;
-  createdAt: string;
+  createdAt?: Date;
 }
 
 // CRUD endpoints for diary app
@@ -16,16 +16,48 @@ export async function diaryRouter(app: FastifyInstance) {
     reply.status(StatusCodes.OK).send(dynamicDbData);
   });
 
+  app.get("/:id", async (request, reply) => {
+    const { id } = request.params as { id: string };
+    const idInteger = parseInt(id, 10); // Cast to integer decimal number
+
+    const diary = dynamicDbData.find((item: Diary) => item.id === idInteger);
+
+    if (!diary) {
+      console.info("Note entry not found");
+      reply.status(StatusCodes.NOT_FOUND).send({
+        message: "Note entry not found",
+      });
+    } else {
+      reply.status(StatusCodes.OK).send(diary);
+    }
+  });
+
   app.post("/", async (request, reply) => {
-    const { body } = request;
+    const body = request.body as Diary;
+
+    if (!body.title || !body.content) {
+      reply.status(StatusCodes.BAD_REQUEST).send();
+      return;
+    }
+
+    body.id = dynamicDbData.length + 1;
+
+    if (!body.createdAt) {
+      body.createdAt = new Date();
+    }
+
     dynamicDbData.push(body);
     reply.status(StatusCodes.CREATED).send(body);
   });
 
   app.patch("/:id", async (request, reply) => {
-    const { id } = request.params as Diary;
-    const { body } = request;
-    const index = dynamicDbData.findIndex((item: Diary) => item.id === id);
+    const body = request.body as Diary;
+    const { id } = request.params as { id: string };
+    const idInteger = parseInt(id, 10); // Cast to integer decimal number
+
+    const index = dynamicDbData.findIndex(
+      (item: Diary) => item.id === idInteger
+    );
     if (index === -1) {
       reply.status(StatusCodes.NOT_FOUND).send();
     } else {
@@ -36,12 +68,17 @@ export async function diaryRouter(app: FastifyInstance) {
 
   app.delete("/:id", async (request, reply) => {
     const { id } = request.params as { id: string };
-    const index = dynamicDbData.findIndex((item: Diary) => item.id === id);
+    const index = dynamicDbData.findIndex(
+      (item: Diary) => item.id === parseInt(id)
+    );
+
     if (index === -1) {
       reply.status(StatusCodes.NOT_FOUND).send();
     } else {
       dynamicDbData.splice(index, 1);
-      reply.status(StatusCodes.NO_CONTENT).send();
+      reply.status(StatusCodes.OK).send({
+        message: "Note entry deleted",
+      });
     }
   });
 }
